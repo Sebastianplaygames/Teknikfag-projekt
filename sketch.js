3086ce2a2a3726554dd71a892ec3ade606b5d4e9
Sebastianpleygames <sebastianpleygames.dk@gmail.com>
2024-04-15 09:49:17 +0200

    farmer

--- sketch.js
+++ sketch.js
@@ -1,16 +1,42 @@
-let player,player2, floor, test;
+let player,player2, floor, test, gras;
 let playerimg;
+
 function preload(){
+  /*
+  gras = loadImage("GRASS+.png")
+  grass = new Group();
+  grass.collider = "none";
+  grass.spriteSheet = gras;
+  grass.addAni({w:16, h:16, row:1, col:1});
+  grass.tile = 'g';
+  grass.w = 20;
+  grass.h = 20;
+  */
   baggrund = new Sprite(0,30)
-   baggrund.collider = 'none';
+  baggrund.collider = 'none';
   baggrund.img = 'funny.png'
   baggrund.diameter = 10
   baggrund.scale = 0.25
   
-  //(48,30)
+  
+  //41, 44
+  farmer = new Sprite(20, 20,68.33, 73.33)
+  farmer.spriteSheet = 'farmer1.png'; 
+  farmer.collider = "none"
+ // farmer.diameter = 32
+  farmer.anis.offset.x = 2;
+  farmer.anis.frameDelay = 8;
+  farmer.scale = 0.6
+  	farmer.addAnis({
+		stand: { row: 0, frames:8 }
+	});
+  	farmer.changeAni('stand');
+
+  
+
   
   player = new Sprite(20, 20,32, 32)
-  player.spriteSheet = 'questKid.png';
+  player.spriteSheet = 'questKid.png'; 
   player.debug = (0,0,30,10);
 player.diameter = 32
   player.anis.offset.x = 2;
@@ -33,9 +59,32 @@ function setup() {
 	new Canvas(600, 340);
   	allSprites.pixelPerfect = true;
 	
+  /*
+    new Tiles(
+      [
+        'g               g',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        '              ',
+        'g               g',
+        10,50, //x, y
+        grass.w, grass.h
   
-
-  
+      ]
+    )
+  */
 	
 	floor = new Sprite(250, 200, 500, 40, 'static');
     test = new Sprite(100, 100, 50,50, 'static');
